f9fb2ea0ead7bac9688bdfe9c4b9427a84e21c8d
Masud <wubetumasud@gmail.com>
2025-11-29 11:12:19 +0300

    patterns dealing with asynchronous code

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('Reading a user data form the database...');
-}, 2000);
+const user = getUser(1);
+console.log(user);
 console.log('After');
+
+//callBack
+//Promise
+//async/await
+
+function getUser(id){
+    setTimeout(() => {
+        console.log('Reading a user data form the database...');
+    }, 2000);
+    return { id:id, githubUsername: 'masud-wubetu'}
+}
