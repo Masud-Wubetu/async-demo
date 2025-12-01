90a7baa215df194260c54675549aef14358d4a7c
Masud <wubetumasud@gmail.com>
2025-12-01 10:07:27 +0300

    creating settled Promises

--- promise.js
+++ promise.js
@@ -1,12 +1,18 @@
 
-const p = new Promise((resolve, reject) => {
-    //Kick off some async work
-    setTimeout(() => {
-        resolve(1); //pending => resolved, fulfilled
-        reject(new Error('message')); //pending => rejected
-    }, 2000);
-});
+// const p = new Promise((resolve, reject) => {
+//     //Kick off some async work
+//     setTimeout(() => {
+//         resolve(1); //pending => resolved, fulfilled
+//         reject(new Error('message')); //pending => rejected
+//     }, 2000);
+// });
 
-p
-    .then(result => console.log('Result', result))
-    .catch(err => console.log('Error', err.message))
+// p
+//     .then(result => console.log('Result', result))
+//     .catch(err => console.log('Error', err.message))
+
+const p = Promise.resolve({id: 1});
+p.then(result => console.log(result))
+
+const Pr = Promise.reject(new Error('reason for rejected'));
+Pr.catch(error => console.log(error));
