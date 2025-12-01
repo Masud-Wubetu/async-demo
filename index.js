d2558ab5ac0aebcb926dca9c50df0692188734a5
Masud <wubetumasud@gmail.com>
2025-12-01 10:46:08 +0300

    async and await approach

--- index.js
+++ index.js
@@ -14,12 +14,28 @@ console.log('Before');
 //     console.log(commits);
 // }
 
-getUser(1)
-    .then(user => getRepositories(user.gitHubUsername))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('Commits', commits))
-    .catch(err => console.log('Error', err.message));
-    
+//            //promise based approach
+// getUser(1)
+//     .then(user => getRepositories(user.gitHubUsername))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => console.log('Commits', commits))
+//     .catch(err => console.log('Error', err.message));
+
+         // async and await approach
+async  function displayCommits() {
+    try{
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    }
+    catch(err){
+        console.log('Error', err.message);
+    }
+
+}
+displayCommits();
+
 console.log('After');
 
 
@@ -36,7 +52,8 @@ function getRepositories(username){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log("calling GitHub API...");
-            resolve(['repo1', 'repo2', 'repo3']);
+            //resolve(['repo1', 'repo2', 'repo3']);
+            reject(new Error('Could not get the repos'));
         }, 2000)
     });
 }
